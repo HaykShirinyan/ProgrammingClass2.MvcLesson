36b041a156e278f5ec4d129a9437bfaf2a27563f
Hayk Shirinyan <haykshirinyan@gmail.com>
2021-09-05 11:59:01 -0700

    Added Product model and migration for it.

---
+++ ProgrammingClass2.MvcLesson/Data/Migrations/20210905184458_Products.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProgrammingClass2.MvcLesson.Data.Migrations
+{
+    public partial class Products : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ ProgrammingClass2.MvcLesson/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.Models
+{
+    // 1 qayln ayn e, vor petq e Model avelacnenq. Ays depqum modele Product class-n e.
+    // C# model-neri property-nerov sarqvum en SQL table-nere.
+
+    // 2 qayln ayn e, vor petq e ays model-ov ApplicationDbContext-i mej property avelacnenq vore hamarjeq klini database-i table-in.
+    public class Product
+    {
+        // Key attribute nshanakum e Id property-n ays model-i primary key e.
+        // Primary key-i mijocov heshtutyamb gtnum enq mer uzac toxe.
+        [Key]
+        public int Id { get; set; }
+
+        // Required attribute nshanakum e chenq karox apranq sarqel aranc anun talu. Apranqi anune partadir e
+        // StringLength attribute nshanakum e apranqi anune petq e amena shate 50 tar unena.
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+    }
+}
