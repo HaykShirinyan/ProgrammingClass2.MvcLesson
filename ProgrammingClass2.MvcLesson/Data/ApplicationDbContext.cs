c09852c91c3222ba3185705f3c87207e180d6c77
André Arshakian <andrearshakian@gmail.com>
2021-09-12 08:24:40 +0200

    Add Category Table Class

--- ProgrammingClass2.MvcLesson/Data/ApplicationDbContext.cs
+++ ProgrammingClass2.MvcLesson/Data/ApplicationDbContext.cs
@@ -3,11 +3,16 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ProgrammingClass2.MvcLesson.Models;
 
 namespace ProgrammingClass2.MvcLesson.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products { get; set; } 
+
+        public DbSet<Category> Categories { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ ProgrammingClass2.MvcLesson/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+    }
+}
