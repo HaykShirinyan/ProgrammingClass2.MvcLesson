e59b0368adcdada3ad2caf24986501b106d1e085
vrezh <vrezh.papazyan@gmail.com>
2021-09-18 12:56:21 +0400

    Category created

---
+++ ProgrammingClass2.MvcLesson/Controllers/CategoriesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass2.MvcLesson.Data;
+using ProgrammingClass2.MvcLesson.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _context; 
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            List<Category> categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (this.ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var category = _context.Categories.Find(Id);
+
+            if (category != null)
+            {
+                return View(category);
+            }
+            return NotFound();
+        }
+    }
+}
