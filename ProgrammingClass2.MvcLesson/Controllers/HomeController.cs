13b811e57b02731a3ed10094c5a3f76a37908a91
hayk <hayki@LAPTOP-0UMDBP51>
2021-09-19 21:46:26 +0400

    add a new comment.

--- ProgrammingClass2.MvcLesson/Controllers/HomeController.cs
+++ ProgrammingClass2.MvcLesson/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace ProgrammingClass2.MvcLesson.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
