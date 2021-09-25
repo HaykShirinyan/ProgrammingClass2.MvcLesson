b14fa24bd369f38321adc9d3ffd0685038110703
hayk <hayki@LAPTOP-0UMDBP51>
2021-09-25 23:08:15 +0400

    add a comment.

--- ProgrammingClass2.MvcLesson/Data/ApplicationDbContext.cs
+++ ProgrammingClass2.MvcLesson/Data/ApplicationDbContext.cs
@@ -3,14 +3,17 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using ProgrammingClass2.MvcLesson.Models;
 
 namespace ProgrammingClass2.MvcLesson.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Product> Products { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
     }
 }
---
+++ ProgrammingClass2.MvcLesson/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string Name { get; set; }
+
+        [StringLength(270)]
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
