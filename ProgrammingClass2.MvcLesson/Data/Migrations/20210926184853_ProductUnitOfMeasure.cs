7455c707cdfa1a9d59b5752761c88a38231c4885
Hayk Shirinyan <haykshirinyan@gmail.com>
2021-09-26 12:27:30 -0700

    Added UnitOfMeasure to Product.

---
+++ ProgrammingClass2.MvcLesson/Data/Migrations/20210926184853_ProductUnitOfMeasure.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProgrammingClass2.MvcLesson.Data.Migrations
+{
+    public partial class ProductUnitOfMeasure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UnitOfMeasureId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_UnitOfMeasureId",
+                table: "Products",
+                column: "UnitOfMeasureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_UnitOfMeasures_UnitOfMeasureId",
+                table: "Products",
+                column: "UnitOfMeasureId",
+                principalTable: "UnitOfMeasures",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_UnitOfMeasures_UnitOfMeasureId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_UnitOfMeasureId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UnitOfMeasureId",
+                table: "Products");
+        }
+    }
+}
