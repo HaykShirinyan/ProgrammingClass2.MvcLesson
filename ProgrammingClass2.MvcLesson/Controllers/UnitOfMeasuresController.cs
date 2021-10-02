d64f75a40f4511f7fd464f7a0067bd27e0f091a5
vrezh <vrezh.papazyan@gmail.com>
2021-10-02 22:45:02 +0400

    UnitOfMeasure created

---
+++ ProgrammingClass2.MvcLesson/Controllers/UnitOfMeasuresController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass2.MvcLesson.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.Controllers
+{
+    public class UnitOfMeasuresController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfMeasuresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var unitOfMeasures = _context.UnitOfMeasures.ToList();
+            return View(unitOfMeasures);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        } 
+    }
+}
