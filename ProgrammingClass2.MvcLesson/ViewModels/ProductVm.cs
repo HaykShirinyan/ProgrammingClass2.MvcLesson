042a26b36ba11f23ab706a5ef626e97ed832e702
Hayk Shirinyan <haykshirinyan@gmail.com>
2021-10-03 12:36:09 -0700

    Changed the Create and Edit actions in ProductsController to use ProductViewModel.

---
+++ ProgrammingClass2.MvcLesson/ViewModels/ProductVm.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammingClass2.MvcLesson.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.ViewModels
+{
+    public class ProductVm
+    {
+        public Product Product { get; set; }
+        public List<UnitOfMeasure> UnitOfMeasures { get; set; }
+    }
+}
