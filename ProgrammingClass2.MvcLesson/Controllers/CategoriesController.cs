688ff1e21bbdd8c78a9318fd2b734e974eb91883
Lilit Shirinyan <lili.tshavrshyan@gmail.com>
2021-10-08 01:52:37 +0400

    Categories

---
+++ ProgrammingClass2.MvcLesson/Controllers/CategoriesController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass2.MvcLesson.Data;
+using ProgrammingClass2.MvcLesson.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass2.MvcLesson.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (this.ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category != null)
+            {
+                return View(category);
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (this.ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category != null)
+            {
+                return View(category);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = _context.Categories.Find(id);
+
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return NotFound();
+        }
+    }
+} 
+
+
+
